45852bf46ec5d2f6125f292328a2422a42529510
humx <humingxing@wingspan.cn>
2016-05-13 15:42:16 +0800

    用户查询增加手机号

--- src/main/java/com/study/common/page/UserPageRequest.java
+++ src/main/java/com/study/common/page/UserPageRequest.java
@@ -15,42 +15,112 @@ public class UserPageRequest extends BasePageRequest {
 
     private String ids;
 
+    private String mobile;
+
+    /**
+     * Gets mobile.
+     *
+     * @return the mobile
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * Sets mobile.
+     *
+     * @param mobile the mobile
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    /**
+     * Gets ids.
+     *
+     * @return the ids
+     */
     public String getIds() {
         return ids;
     }
 
+    /**
+     * Sets ids.
+     *
+     * @param ids the ids
+     */
     public void setIds(String ids) {
         this.ids = ids;
     }
 
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets type.
+     *
+     * @return the type
+     */
     public Integer getType() {
         return type;
     }
 
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
     public void setType(Integer type) {
         this.type = type;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Gets start.
+     *
+     * @return the start
+     */
     public Integer getStart() {
         return start;
     }
 
+    /**
+     * Sets start.
+     *
+     * @param start the start
+     */
     public void setStart(Integer start) {
         this.start = start;
     }
